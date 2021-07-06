2d6b86d76e528ba303f95f3a08e861dad38c3ad4
gestirn717 <86178320+gestirn717@users.noreply.github.com>
2021-07-06 13:34:21 +0900

    Add files via upload

---
+++ baekjoon_test/test_10998.java
@@ -0,0 +1,16 @@
+package baekjoon_test;
+
+
+import java.util.Scanner;
+public class test_10998 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		System.out.print(a*b);
+		
+	}
+
+}
