1ce50d08e76a09f847d97255d5ba7a89adfbb436
gestirn717 <86178320+gestirn717@users.noreply.github.com>
2021-07-24 14:13:21 +0900

    Update test2_2558.java

--- baekjoon_test/test2_2558.java
+++ baekjoon_test/test2_2558.java
@@ -14,8 +14,8 @@ public class test2_2558 {
 		int b = Integer.parseInt(br.readLine());
 		
 		sb.append(a*(b%10)).append('\n');        // 1의 자리  
-		sb.append(a*(b/10%10)).append('\n');     // 10의자리 
-		sb.append(a*b/100).append('\n');         // 100의 자리 
+		sb.append(a*((b%100)/10)).append('\n');     // 10의자리 
+		sb.append(a*(b/100)).append('\n');         // 100의 자리 
 		sb.append(a*b);                          
 		
 		System.out.print(sb);
